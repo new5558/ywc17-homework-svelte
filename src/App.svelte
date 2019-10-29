6b050a768bcf4e55c181044432d950bb5adc0535
new5558 <new17353@gmail.com>
2019-10-29 14:15:18 +0700

    init project

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from 'svelte'
+  onMount(() => {
+    fetch('https://panjs.com/ywc.json')
+      .then(response => response.json())
+      .then(console.log)
+  })
+  export let name;
+</script>
+
+<style lang="scss">
+  h1 {
+    &:hover {
+      color: red;
+    }
+  }
+</style>
+
+<h1>Hello {name}!</h1>
