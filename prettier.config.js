2733f65a87a9b66bf45230f2408dd321fb1fd2c1
new5558 <new17353@gmail.com>
2019-10-31 00:28:35 +0700

    feat: add SEO, title, favicon

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    tabWidth: 2,
-    semi: false,
-    singleQuote: true,
-    trailingComma: 'es5',
-    plugins: ['svelte'],
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  plugins: ['svelte'],
 }
